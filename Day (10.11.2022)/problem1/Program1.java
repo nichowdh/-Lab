d02cc97782a00011efde47e5ca12f8dd477241f7
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2022-11-11 11:31:55 +0530

    Add files via upload

--- Day (10.11.2022)/problem1/Program1.java
+++ Day (10.11.2022)/problem1/Program1.java
@@ -2,53 +2,20 @@ package problem1;
 
 //Write a program which creates a String “Welcome to Java World” and performs the following 
 public class Program1 {
-	String s1 = "Welcome to Java World";
-	String s2 = "Welcome";
+	public static void main(String[] args) {
 
-	void show() { // method 1, to check 5th position
+		String s1 = "Welcome to Java World";
 		char ch = s1.charAt(5);
 		System.out.println("The Character at 5th position is: " + ch);
+		System.out.println("Compare: " + s1.compareTo("Welcome"));
+		String s2 = s1.concat("- Let us learn");
+		System.out.println("After concat:" + s2);
+		System.out.println("First occurance index of 'a' is:" + s2.indexOf('a'));
+		String s3 = s1.replace('a', 'e');
+		System.out.println("Replace string is: " + s3);
+		System.out.println("Substring is: " + s1.substring(4, 11));
+		System.out.println("updated String after lower case: " + s1.toLowerCase());
+
 	}
 
-	void display() { // 2nd method, to compare lexographically
-		if (s1.equals(s2))
-			System.out.println("Equal");
-		else
-			System.out.println("Not Equal");
-	}
-
-	void concat() { // 3rd method to concat
-		s1 = s1.concat("- Let us learn");
-		System.out.println(s1);
-	}
-
-	void index() {// 4th method to define 1st occurance of a
-		int i = s1.indexOf("a");
-		System.out.println("First occurance of 'a' is: " + i);
-	}
-
-	void replace() { // 5th method to rplace 'a', with 'e'
-		String j = s1.replaceAll("a", "e");
-		System.out.println("Updated String is: " + j);
-	}
-
-	void substring() { // 6th method to Returns string between 4th position and 10th position and
-						// display it
-		System.out.println("Substring is: " + s1.substring(4, 10));
-	}
-
-	void lowercase() {// 7th method to Returns the lowercase of the string and display it
-		System.out.println("Updated string after lowercase: " + s1.toLowerCase());
-	}
-
-	public static void main(String[] args) { // main methods
-		Program1 obj = new Program1(); // object creation
-		obj.show(); // calling 1st method
-		obj.display();// calling 2nd method
-		obj.concat();// calling 3rd method
-		obj.index();// calling 4th method
-		obj.replace();// calling 5th method
-		obj.substring();// calling 6th method
-		obj.lowercase();// calling 7th method
-	}
 }
--- Day (10.11.2022)/problem1/Program3.java
+++ Day (10.11.2022)/problem1/Program3.java
@@ -1,26 +1,13 @@
 package problem1;
 //Write a program which creates a String “C:\IBM\DB2\PROGRAM\DB2COPY1.EXE”. It parses the string with the delimiter as ‘\’ and displays the String in the following format.
 
-//Drive: c:\
-
-import java.util.StringTokenizer;
-
 public class Program3 {
-	public static void show() {
-		StringTokenizer str = new StringTokenizer("C:\\IBM\\DB2\\PROGRAM\\DB2COPY1.EXE");
-		while (str.hasMoreTokens()) { // boolean method to checks if there is more token availale or not
-			System.out.println(str.nextToken("\\"));
-			StringTokenizer str1 = new StringTokenizer(" ");
-			while (str1.hasMoreTokens()) {// String method, its returns the next token from String tokenizer
-				// object
-				System.out.println();
-				System.out.println(str1.nextToken("||"));
-			}
-		}
-	}
-
 	public static void main(String[] args) {
-		show();
-	}
+		StringBuilder str = new StringBuilder("C:\\IBM\\DB2\\PROGRAM\\DB2COPY1.EXE");
+		System.out.println("Drive:" + str.substring(0, 3));
+		System.out.println("Folder:" + str.substring(3, 6).concat("||") + str.substring(7, 10).concat("||")
+				+ str.substring(11, 18));
+		System.out.println("File:" + str.substring(20));
 
+	}
 }

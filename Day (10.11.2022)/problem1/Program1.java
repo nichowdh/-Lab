617c6d951957e930ee4243756b81495f44f3079d
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2022-11-11 11:40:47 +0530

    Add files via upload

--- Day (10.11.2022)/problem1/Program1.java
+++ Day (10.11.2022)/problem1/Program1.java
@@ -1,20 +1,20 @@
 package problem1;
 
 //Write a program which creates a String “Welcome to Java World” and performs the following 
-public class Program1 {
-	public static void main(String[] args) {
+public class Program1 {// calss
+	public static void main(String[] args) { // main method
 
 		String s1 = "Welcome to Java World";
-		char ch = s1.charAt(5);
+		char ch = s1.charAt(5); // find the character at 5th position
 		System.out.println("The Character at 5th position is: " + ch);
-		System.out.println("Compare: " + s1.compareTo("Welcome"));
-		String s2 = s1.concat("- Let us learn");
+		System.out.println("Compare: " + s1.compareTo("Welcome")); // comparing the new string
+		String s2 = s1.concat("- Let us learn"); // concatinating new string
 		System.out.println("After concat:" + s2);
-		System.out.println("First occurance index of 'a' is:" + s2.indexOf('a'));
-		String s3 = s1.replace('a', 'e');
+		System.out.println("First occurance index of 'a' is:" + s2.indexOf('a')); // find the first occurance of 'a'
+		String s3 = s1.replace('a', 'e'); // replacing the character 'a' with 'e'
 		System.out.println("Replace string is: " + s3);
 		System.out.println("Substring is: " + s1.substring(4, 11));
-		System.out.println("updated String after lower case: " + s1.toLowerCase());
+		System.out.println("updated String after lower case: " + s1.toLowerCase()); // converting to the lowercase
 
 	}
 
--- Day (10.11.2022)/problem1/Program3.java
+++ Day (10.11.2022)/problem1/Program3.java
@@ -1,13 +1,13 @@
 package problem1;
 //Write a program which creates a String “C:\IBM\DB2\PROGRAM\DB2COPY1.EXE”. It parses the string with the delimiter as ‘\’ and displays the String in the following format.
 
-public class Program3 {
-	public static void main(String[] args) {
-		StringBuilder str = new StringBuilder("C:\\IBM\\DB2\\PROGRAM\\DB2COPY1.EXE");
-		System.out.println("Drive:" + str.substring(0, 3));
+public class Program3 { // class
+	public static void main(String[] args) { // main method
+		StringBuilder str = new StringBuilder("C:\\IBM\\DB2\\PROGRAM\\DB2COPY1.EXE");// string builder class
+		System.out.println("Drive:" + str.substring(0, 3)); // printing 'Drive:C:\'
 		System.out.println("Folder:" + str.substring(3, 6).concat("||") + str.substring(7, 10).concat("||")
-				+ str.substring(11, 18));
-		System.out.println("File:" + str.substring(20));
+				+ str.substring(11, 18)); // printing 'Folder:IBM||DB2||PROGRAM'
+		System.out.println("File:" + str.substring(20)); // printing 'File:B2COPY1.EXE'
 
 	}
 }

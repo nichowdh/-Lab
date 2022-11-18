24b11fc5db78dd55456a30b4cfe75be6d13abf22
Niladri Chowdhury <111556280+nichowdh@users.noreply.github.com>
2022-11-18 13:42:27 +0530

    Add files via upload

--- Lab (17.11.2022)/ProblemCode2.java
+++ Lab (17.11.2022)/ProblemCode2.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class ProblemCode2 { // class
 
-	public static void main(String[] args) { // main method
+	public static void main(String[] args) throws Exception { // main method
 		int x, y; // integer value
 		Scanner sc = new Scanner(System.in); // user input
 		System.out.println("Enter 2 numbers");
 		x = sc.nextInt();
 		y = sc.nextInt();
 		try {
-			if (x < 0 || y < 0) { // if condition
-				System.out.println("Exception Occure");
+			if ((x >= -20 || y <= 20) && (x < 0 || y < 0)) { // if condition
+				throw new Exception("X & Y should not be zero");
 			} else
-				System.out.println(x * y);
+				System.out.println("Product: " + x * y);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
